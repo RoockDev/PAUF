91fa6caa60eabf43bb35d0c7aef494a272b400f3
alion1992 <55086590+alion1992@users.noreply.github.com>
2025-09-15 18:55:43 +0200

    Update log_nombres_fichero.py
    CAMBIALO

--- Ud1/ejercicios_ficheros/log_nombres_fichero.py
+++ Ud1/ejercicios_ficheros/log_nombres_fichero.py
@@ -23,3 +23,16 @@ with open("Libro2.txt", "r") as fichero_entrada:
             insert_sql = f'Insert into Personas (id,nombre,apellidos,fecha_nacimiento,calle,codigo_postal,numero,movil) values (seq_personas.nextval, "{nombre}", "{apellidos}", "{fecha_nacimiento}". "{calle}", "{codigo_postal}", "{numero}", "{movil}")'
 
             fichero_salida.write(insert_sql + "\n")
+
+
+#ahora = datetime.now().strftime("%d%m%Y") o como si lo haces todo en la linea 5. no me crees dos variables para hacer una cosa que puedes hacer una.
+'''
+nombre = datos[0]
+apellidos = datos[1]
+fecha_nacimiento = datos[2]
+calle = datos[3]
+codigo_postal = datos[4]
+numero = datos[5]
+movil = datos[6]
+'''
+#Seguro que se te ocurre un metodo que haga algo mejor que eso por si te cambio el txt no puedes hacer uno general? y con el f en la línea 23 si pones comillas dobles no tienes que poner comillas cada vez que pongas {x}
