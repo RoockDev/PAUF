478b547b90d04a7571a94607b3af1bdea08ae037
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-22 01:28:33 +0200

    Django RadioApp CRUD MODELO USUARIO

--- Ud2/DjangoRadio-master/DjangoRadio-master/RadioVirgenDeGracia2026/radioapp/urls.py
+++ Ud2/DjangoRadio-master/DjangoRadio-master/RadioVirgenDeGracia2026/radioapp/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls.i18n import urlpatterns
 from django.urls import path
 from .import views
 urlpatterns = [
+    #CANCION
     path('obtenerCancion/<int:id>/',views.get_cancion),
      path('add_cancion/',views.add_cancion),
      path('get_canciones/',views.get_canciones),
     path('delete_cancion/<int:id>/',views.delete_cancion),
-    path('update_cancion/<int:id>/',views.update_cancion)
+    path('update_cancion/<int:id>/',views.update_cancion),
+    #USUARIO
+    path('get_usuarios/',views.get_usuarios),
+    path('get_usuario/<int:id>/',views.get_usuario),
+    path('add_usuario/',views.add_usuario),
+    path('delete_usuario/<int:id>/',views.delete_usuario),
+    path('update_usuario/<int:id>/',views.update_usuario)
+
+
 ]
--- Ud2/DjangoRadio-master/DjangoRadio-master/RadioVirgenDeGracia2026/radioapp/views.py
+++ Ud2/DjangoRadio-master/DjangoRadio-master/RadioVirgenDeGracia2026/radioapp/views.py
@@ -1,4 +1,6 @@
 import json
+from getpass import fallback_getpass
+from idlelib.pyshell import usage_msg
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -54,6 +56,62 @@ def update_cancion(request,id):
 
     except Cancion.DoesNotExist:
         return JsonResponse({"error": "cancion no encontrada"},status=404)
+
+
+#----------CRUD USUARIO----------
+#get obtener todos los usuarios
+def get_usuarios(request):
+    usuarios = list(Usuario.objects.all().values())
+    return JsonResponse(usuarios,safe=False)
+
+#get obtener un usuario por id
+def get_usuario(request,id):
+    try:
+        usuario = Usuario.objects.values().get(pk=id)
+        return JsonResponse(usuario)
+    except Usuario.DoesNotExist:
+        return JsonResponse({"error":" usuario no encontrado"},status=404)
+
+#post crear un usuario
+@csrf_exempt
+def add_usuario(request):
+    if request.method == 'POST':
+        try:
+            jsonUsuario = json.loads(request.body)
+            usuario = Usuario.objects.create(**jsonUsuario)
+            return JsonResponse({"Succes":"Usuario creado correctamente"},status=201)
+        except Exception as e:
+            return JsonResponse({"error": str(e)},status=400)
+
+#delete borrar un usuario
+@csrf_exempt
+def delete_usuario(request,id):
+    if request.method == 'DELETE':
+        try:
+            usuario = Usuario.objects.get(pk=id)
+            usuario.delete()
+            return  JsonResponse({"succes": "Usuario borrado correctamente"})
+        except Usuario.DoesNotExist:
+            return  JsonResponse({"Succes": "Usuario no encontrado"})
+#update updatear un usuario
+@csrf_exempt
+def update_usuario(request,id):
+    if request.method == 'PUT':
+        try:
+            usuario = Usuario.objects.get(pk=id)
+            nuevos_datos = json.loads(request.body)
+
+            usuario.nombre = nuevos_datos.get('nombre',usuario.nombre)
+            usuario.apellido = nuevos_datos.get('apellido',usuario.apellido)
+            usuario.nick = nuevos_datos.get('nick',usuario.nick)
+            usuario.fecha_nacimiento = nuevos_datos.get('fecha_nacimiento', usuario.fecha_nacimiento)
+            usuario.telefono = nuevos_datos.get('telefono',usuario.telefono)
+            usuario.email = nuevos_datos.get('email',usuario.email)
+            usuario.activo = nuevos_datos.get('activo',usuario.activo)
+            return JsonResponse({"mensaje":"usuario actualizado correctamente"})
+        except Usuario.DoesNotExist:
+            return JsonResponse({"error":"usuario no encontrado"})
+
 
 
 
